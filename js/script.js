6bc3dd3dd771fdeb85c14c43986da84d25fdb18c
ElenaRyser <elena.ryser@stud.fhgr.ch>
2026-05-05 10:08:18 +0200

    API einbinden

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("blub");
+// Zugriff auf Color RGB API
+async function loadData() {
+    const url = 'https://www.thecolorapi.com/id?rgb=rgb(0,71,171)'; // mit korrekter API-URL ersetzen
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const data = await loadData();
+console.log(data); // gibt die Daten der API oder false in der Konsole aus

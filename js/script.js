5483dd0c835910948ebb64834313386e2ced3e4e
ElenaRyser <elena.ryser@stud.fhgr.ch>
2026-05-09 08:46:33 +0200

    Test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Zugriff auf Color RGB API
 async function loadData() {
-    const url = 'https://www.thecolorapi.com/id?rgb=rgb(0,71,171)'; // mit korrekter API-URL ersetzen
+    const url = 'https://www.thecolorapi.com/id?rgb=rgb(255,152,216)'; // mit korrekter API-URL ersetzen
     try {
         const response = await fetch(url);
         return await response.json();
@@ -11,4 +11,5 @@ async function loadData() {
 }
 const data = await loadData();
 console.log(data); // gibt die Daten der API oder false in der Konsole aus
+//console.log(data.cmyk.value);
 
